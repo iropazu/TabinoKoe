456cc867bf522e0790cf1148ae7919b91b4a6794
Takato <c1342880@st.kanazawa-it.ac.jp>
2024-11-08 13:42:31 +0900

    a

--- app.py
+++ app.py
@@ -3,18 +3,25 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
 
 #定義
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 db = SQLAlchemy(app)
+
 
 #db作成
 
-class Questionnaire(db.Model):
+class Question_images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quesitonnaire_result = db.Column(db.String(50) , unique = True)
+    place_name = db.Column(db.String(10), nullable = False)
+    image_file = db.Column(db.String(100), nullable = False)
 
 ##アンケート部分
 
@@ -32,9 +39,24 @@ def information():
 
 #page3
 
-@app.route("/useself")
-def useself():
-    return render_template("useself.html")
+@app.route("/add_images", methods = ["POST"])
+def add_images():
+    #db追加
+    #場所の名前
+    placename = request.form.get("place_name")
+
+    #画像データ
+    imagefile = request.files.get("image_file")
+    filename = secure_filename(imagefile.filename)
+    imagefile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+    #db格納
+    user_new_date = Question_images(place_name = placename, image_file = filename)
+    db.session.add(user_new_date)
+    db.session.commit()
+
+    return render_template("choice.html")
 
 #page4
 
